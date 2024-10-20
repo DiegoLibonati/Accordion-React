93360b10028c93b31e43312e67e94c95b61911f3
Die <dlibonati@leafnoise.io>
2024-10-20 20:06:22 -0300

    patch: main component removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Main } from "./components/Main";
+import { useState } from "react";
+
+import { Question as QuestionT } from "./entities/entities";
+
+import { Question } from "./components/Question";
+
+import questions from "./helpers/data";
 
 import "./App.css";
 
 function App(): JSX.Element {
-  return <Main></Main>;
+  const [question] = useState<QuestionT[]>(questions);
+
+  return (
+    <main className="w-screen h-screen bg-[#fafafa]">
+      <section className="w-full h-full">
+        <article className="flex items-center justify-center flex-col w-full h-full">
+          {question.map((question) => {
+            return <Question key={question.id} {...question}></Question>;
+          })}
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
